2bcaba3a92f74d04054819dda77a21abd24256f3
Fabio <35787819+wdvlpr@users.noreply.github.com>
2022-11-23 12:00:03 -0300

    updates commit

--- save.php
+++ save.php
@@ -1,7 +1,7 @@
 <?php
-$img = $_POST['image'];
-$img = str_replace('data:image/png;base64,', '', $img);
-$img = str_replace(' ', '+', $img);
-$data = base64_decode($img);
+$assinatura = $_POST['image'];
+$assinatura = str_replace('data:image/png;base64,', '', $assinatura);
+$assinatura = str_replace(' ', '+', $assinatura);
+$data = base64_decode($assinatura);
 file_put_contents(time().'.png', $data);
 ?>
