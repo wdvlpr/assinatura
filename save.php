af328a567be7c1f9f558b1af1f3b1ab5d7f64dc3
Fabio <35787819+wdvlpr@users.noreply.github.com>
2022-11-25 10:04:33 -0300

    updates

--- save.php
+++ save.php
@@ -3,5 +3,24 @@ $assinatura = $_POST['image'];
 $assinatura = str_replace('data:image/png;base64,', '', $assinatura);
 $assinatura = str_replace(' ', '+', $assinatura);
 $data = base64_decode($assinatura);
-file_put_contents(time().'.png', $data);
+$time = time();
+file_put_contents($time.'.png', $data);
+
+require('fpdf/fpdf.php');
+
+$pdf = new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(0,10,'Recibo!',0,1,'C');
+$pdf->SetFont('Arial','',12);
+$pdf->Cell(0,10,'Texto do recibo.',0,1,'L');
+
+$pdf->Image($time.'.png', 0, 0, 300, 150);
+
+$pdf->Cell(0,10,'_________________________');
+//$pdf->Output();
+$pdf->Output('F', $time.'.pdf');
+
+echo $time;
+
 ?>
